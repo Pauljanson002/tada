a1f91f25bb8388d8231f60101f148b6b4eb431b5
Paul Janson <pauljanson002@gmail.com>
2023-12-13 13:07:54 -0800

    nothing important just run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python -m apps.run --config configs/tada_wo_dpt.yaml --text "man" --action "running" 
+CUDA_VISIBLE_DEVICES=0 python -m apps.run --config configs/tada_wo_dpt.yaml --text "man" --action "sitting" 
+CUDA_VISIBLE_DEVICES=0 python -m apps.run --config configs/tada_wo_dpt.yaml --text "man" --action "walking" 
