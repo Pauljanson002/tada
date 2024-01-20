7ad6fa8940e6948f9d7e6840ae351412ee89a934
Paul Janson <pauljanson002@gmail.com>
2024-01-19 19:09:49 -0500

    removed mediapipe dependency of the trainer.py

--- submit.py
+++ submit.py
@@ -2,14 +2,14 @@ import os
 import submitit
 
 def main():
-    os.system('python -m apps.run text="man" action="running" wandb_mode="disabled" name=six_d_use_6d_True_model_change_False model.use_6d=True model.model_change=False training.debug=True')
+    os.system('python -m apps.run text="man" action="running" wandb_mode="online" name=six_d_use_6d_True_model_change_False model.use_6d=True model.model_change=False training.debug=True')
     print("Done")
     return True
     
 
 log_folder = "/scratch/janson2/tmp"
 executor = submitit.AutoExecutor(folder=log_folder)
-executor.update_parameters(timeout_min=60,slurm_account="rrg-eugenium",gpus_per_node=1)
+executor.update_parameters(timeout_min=4,slurm_account="rrg-eugenium",gpus_per_node=1,mem_gb=32)
 
 job = executor.submit(main) 
 
