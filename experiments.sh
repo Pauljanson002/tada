a3e8fc64f1acec53d8bedc0880222cfee9293466
Paul Janson <pauljanson002@gmail.com>
2024-01-22 14:42:56 -0500

    changed_iters to 1000

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action="running" wandb_mode="online" name=full_pose__model_change_True_full_pose_True model.model_change=True model.use_full_pose=True
-CUDA_VISIBLE_DEVICES=1 python -m apps.run text="man" action="running" wandb_mode="online" name=full_pose__model_change_True_full_pose_False model.model_change=True model.use_full_pose=False
-CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action="running" wandb_mode="online" name=full_pose__model_change_False_full_pose_True model.model_change=False model.use_full_pose=True
-CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action="running" wandb_mode="online" name=full_pose__model_change_False_full_pose_False model.model_change=False model.use_full_pose=False
+CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action=running wandb_mode="online" name=action__model_change_True_full_pose_False_action_running model.model_change=True model.use_full_pose=False
+CUDA_VISIBLE_DEVICES=1 python -m apps.run text="man" action=sitting wandb_mode="online" name=action__model_change_True_full_pose_False_action_sitting model.model_change=True model.use_full_pose=False
+CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action=jumping wandb_mode="online" name=action__model_change_True_full_pose_False_action_jumping model.model_change=True model.use_full_pose=False
--- generate.py
+++ generate.py
@@ -1,9 +1,10 @@
 model_changes = [True, False]
 use_full_pose = [True, False]
 actions = ["running", "sitting","jumping"]
-for model_change in model_changes:
-    for full_pose in use_full_pose:
-        name = f"full_pose__model_change_{model_change}_full_pose_{full_pose}"
-        script = f'CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action="running" wandb_mode="online" name={name} model.model_change={model_change} model.use_full_pose={full_pose}'
-        print(script)
+for act in actions:
+    model_change = True
+    full_pose = False
+    name = f"action__model_change_{model_change}_full_pose_{full_pose}_action_{act}"
+    script = f'CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action={act} wandb_mode="online" name={name} model.model_change={model_change} model.use_full_pose={full_pose}'
+    print(script)
     # Execute the script or save it for later use
