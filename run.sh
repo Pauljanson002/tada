6e52c68fbffc49d5c19cb6e000b3c604d5e10473
Paul Janson <pauljanson002@gmail.com>
2024-01-29 11:43:36 -0500

    run.sh change

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action="running" wandb_mode="disabled" model.model_change=True model.use_full_pose=False name=test training.debug=False guidance.name=zeroscope model.video=True model.num_frames=6
+CUDA_VISIBLE_DEVICES=0 python -m apps.run text="man" action="running" wandb_mode="disabled" model.model_change=True model.use_full_pose=False name=test training.debug=True guidance.name=zeroscope model.video=True model.num_frames=6
 # CUDA_VISIBLE_DEVICES=0 python -m apps.run --config configs/tada_wo_dpt.yaml --text "man" --action "sitting" 
 # CUDA_VISIBLE_DEVICES=0 python -m apps.run --config configs/tada_wo_dpt.yaml --text "man" --action "walking" 
