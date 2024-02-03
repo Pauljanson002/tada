6a46a9109d8d4bc4405d292ef541815f5710359e
Paul Janson <pauljanson002@gmail.com>
2024-02-02 20:44:45 -0500

    compute canada changes

---
+++ cc.sh
@@ -0,0 +1,2 @@
+python -m apps.run --multirun --config-name=submitit_slurm  text="man" action="running" wandb_mode="disabled" name=reg_acc_t_running training.debug=False guidance.name=zeroscope model.video=True model.num_frames=10 training.iters=1000 training.accumulate=True training.regularize_coeff=1000,100,10,1,0.1
+#python -m apps.run --multirun text="man" action="running" wandb_mode="disabled" name=reg_acc_f training.debug=False guidance.name=zeroscope model.video=True model.num_frames=14 training.iters=1000 training.accumulate=False training.regularize_coeff=1000,100,10,1,0.1
