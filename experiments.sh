83ac4c7fbfddfba422ced4141d81667453828148
Paul Janson <pauljanson002@gmail.com>
2024-05-22 16:53:28 -0700

    experimental setups

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python -m apps.run --multirun +experiment=dg_native_resol_low_view
-CUDA_VISIBLE_DEVICES=0 python -m apps.run --multirun +experiment=dg_native_resol
+CUDA_VISIBLE_DEVICES=0 python -m apps.run --multirun +experiment=dg_2_after_best
